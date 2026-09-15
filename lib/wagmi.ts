9d5ff2ad2cf276958fc7e4adb9bb5670d13fe783
ezedavidt54-ux <ezedavidt54@gmail.com>
2026-09-15 05:23:36 +0100

    Build custom KYOX wallet connector list

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -1,9 +1,28 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import {
+  base as baseWallet,
+  binanceWallet,
+  injectedWallet,
+  metaMaskWallet,
+  phantomWallet,
+  rainbowWallet,
+  walletConnectWallet,
+} from '@rainbow-me/rainbowkit/wallets';
 import { arbitrum, base, mainnet } from 'wagmi/chains';
 
 export const wagmiConfig = getDefaultConfig({
   appName: 'KYOX',
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID ?? '',
   chains: [arbitrum, base, mainnet],
+  wallets: [
+    {
+      groupName: 'Recommended',
+      wallets: [metaMaskWallet, baseWallet, binanceWallet, phantomWallet, rainbowWallet],
+    },
+    {
+      groupName: 'More ways to connect',
+      wallets: [walletConnectWallet, injectedWallet],
+    },
+  ],
   ssr: true,
 });
